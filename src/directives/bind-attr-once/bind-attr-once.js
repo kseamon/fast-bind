1655849af56e0c2cb487a7b92e1c62c746935677
E Brent Nelson <brent@ebrentnelson.com>
2014-02-09 23:31:24 -0800

    Use the right attributes.
    Used the wrong ones.

--- src/directives/bind-attr-once/bind-attr-once.js
+++ src/directives/bind-attr-once/bind-attr-once.js
@@ -12,11 +12,11 @@ angular.module('fastBind.bindAttrOnce', []).
       compile: function compile(element, attributes) {
         var expr = $parse(attributes.fastBindAttrOnce);
 
-        return function link(scope, element) {
+        return function link(scope, element, attrs) {
           var values = expr(scope);
 
           angular.forEach(values, function(value, key) {
-            attributes.$set(key, value);
+            attrs.$set(key, value);
           });
         };
       }
